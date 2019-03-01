c8e5075a80360063fecdc84ed26539167d1810a0
huajie chen <cmathking@gmail.com>
2019-03-01 11:38:32 +0800

    Update 第28题_字符串的全排列.py

--- 剑指offer/第28题_字符串的全排列.py
+++ 剑指offer/第28题_字符串的全排列.py
@@ -1,33 +1,23 @@
 # -*- coding:utf-8 -*-
 """
 问题：输入一个字符串，打印该字符串中字符的所有排列
-思路：不使用递归
+思路：交换位置，递归
 """
 
-def permutation(string):
-    if string is None:
-        return
-    
-    count = 0
-    print(string)
-    count += 1
-    if len(string) < 2:
-        return
-    
-    chars = string
-    i = 0
-    string = string[:i] + string[i+1] + string[i] + string[i+2:]
-    i += 1
-    while string != chars:
-        print(string)
-        count += 1
-        if i == len(string) - 1:
-            string = string[i] + string[1:i] + string[0]
-            i = 0
-        else: 
-            string = string[:i] + string[i+1] + string[i] + string[i+2:]
-            i += 1
+def permutation(char_arr):
+    if len(char_arr) == 1:
+        return [char_arr]
+    all_res = []
+    for i in range(0, len(char_arr)):
+        char_arr[0], char_arr[i] = char_arr[i], char_arr[0]
+        res = permutation(char_arr[1:])
+        for r in res:
+            all_res.append(char_arr[0] + ''.join(r))
+        char_arr[0], char_arr[i] = char_arr[i], char_arr[0]
+    return all_res
 
-permutation("abc")
+
+s = "5462"
+print(permutation(list(s)))
     
 
